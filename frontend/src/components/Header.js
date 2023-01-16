e19ce8d437e7f4b73241d20932c4bd3402666902
Andres Gutierrez <12andresgutierrez04@gmail.com>
2023-01-15 21:25:27 -0400

    Updating docker compose for production

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="flex flex-row space-between bg-cyan-900 text-white py-6 px-10">
-      <h1 className="font-bold text-xl">TradeManager</h1>
+      <h1 className="font-bold text-xl">TradeManagerasdsa</h1>
     </header>
   );
 }
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from main import app
+
+# do some production specific things to the app
+if __name__ == "__main__":
+    app.config['DEBUG'] = False
+    app.run()
