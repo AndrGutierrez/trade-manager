4f80ecad5a490f50da73e0e0972515e2a6f8cb86
Andres Gutierrez <12andresgutierrez04@gmail.com>
2023-10-26 00:45:51 -0400

    bugfixing docker compose and installing dependencies for login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,22 @@
-import React from "react"
-export default function Login(){
-	const handleChange = () =>{
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-	}
-	return (
-		<div className="flex align-center">
-					<h1>Hello I am actopikant not</h1>
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data:Object) => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
 
-		</div>
-	)
+      <input {...register("Developer", { required: true })} type="radio" value="Yes" />
+      <input {...register("Developer", { required: true })} type="radio" value="No" />
+
+      <input type="submit" />
+    </form>
+  );
 }
